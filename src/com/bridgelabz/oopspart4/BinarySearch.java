2ba53845aba106118cd5d9725b1e0686e459d681
ashishmashal <ashishmashal5@gmail.com>
2022-05-16 20:49:41 +0530

    Added:Welcome Messege Added for Binary Search

--- src/com/bridgelabz/oopspart4/BinarySearch.java
+++ src/com/bridgelabz/oopspart4/BinarySearch.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.oopspart4;
 
-public class HashTable {
+public class BinarySearch {
     public static void main(String[] args) {
-        System.out.println("Welcome To HashTable");
+        System.out.println("Welcome To Binary Search");
     }
 }
